6ef186f5b3327947902e5cb737622db78a0ca777
kene-ops <okongwu.k@northeastern.edu>
2026-01-13 14:01:25 -0800

    lab1 homework finish
    my finished lab1 work

---
+++ lab1/temp-conversion.js
@@ -0,0 +1,9 @@
+let fahrenheit = 81 //this is the temperature in enugu state, nigeria, in march 2023
+
+let celsuis = (fahrenheit - 32) * 5 / 9
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+console.log ("Temperature kelvin" + ': ' + kelvin)
+console.log ("Temperature celsuis" + ': ' + celsuis)
+console.log ("Temperature fehrenheit" + ': ' + fahrenheit)
+
