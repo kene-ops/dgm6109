745f6c6d0440ffe9f5a8dccd57b6cfac4a4241b4
kene-ops <okongwu.k@northeastern.edu>
2026-02-14 13:58:16 -0800

    termphase2

---
+++ term/phase2/main.js
@@ -0,0 +1,236 @@
+"use strict"
+
+/*
+Hypothesis:
+As my gym/workout level decreases, my productivity level decreases.
+
+This file contains the structured dataset for my term project.
+The top-level data structure is an Array of daily observations.
+Each observation is represented as an Object.
+*/
+
+let observations = [{
+    date: "2026-01-23",
+    sleepMinutes: 400,
+    meditationMinutes: 15,
+    meals: 2,
+    gym: {
+        weightKg: 85,
+        reps: 210
+    }, // gym workout details
+    productivity: {
+        tasksCompleted: 3,
+        tasksSet: 4,
+        remark: "Productive"
+    } // productivity results
+}, // one daily observation
+
+{
+    date: "2026-01-24",
+    sleepMinutes: 345,
+    meditationMinutes: 16,
+    meals: 2,
+    gym: {
+        weightKg: 85,
+        reps: 180
+    },
+    productivity: {
+        tasksCompleted: 2,
+        tasksSet: 4,
+        remark: "Fairly productive"
+    }
+},
+
+{
+    date: "2026-01-26",
+    sleepMinutes: 305,
+    meditationMinutes: 15,
+    meals: 1,
+    gym: {
+        weightKg: null,
+        reps: null
+    },
+    productivity: {
+        tasksCompleted: 1,
+        tasksSet: 3,
+        remark: "Poorly productive"
+    }
+},
+
+{
+    date: "2026-01-27",
+    sleepMinutes: 330,
+    meditationMinutes: 14,
+    meals: 2,
+    gym: {
+        weightKg: 87,
+        reps: 200
+    },
+    productivity: {
+        tasksCompleted: 2,
+        tasksSet: 3,
+        remark: "Productive"
+    }
+},
+
+{
+    date: "2026-01-28",
+    sleepMinutes: 420,
+    meditationMinutes: 15,
+    meals: 3,
+    gym: {
+        weightKg: 90,
+        reps: 240
+    },
+    productivity: {
+        tasksCompleted: 3,
+        tasksSet: 3,
+        remark: "Very productive"
+    }
+},
+
+{
+    date: "2026-01-29",
+    sleepMinutes: 240,
+    meditationMinutes: 18,
+    meals: 2,
+    gym: {
+        weightKg: null,
+        reps: null
+    },
+    productivity: {
+        tasksCompleted: 3,
+        tasksSet: 4,
+        remark: "Productive"
+    }
+},
+
+{
+    date: "2026-01-31",
+    sleepMinutes: 420,
+    meditationMinutes: 15,
+    meals: 2,
+    gym: {
+        weightKg: 82,
+        reps: 80
+    },
+    productivity: {
+        tasksCompleted: 2,
+        tasksSet: 2,
+        remark: "Very productive"
+    }
+},
+
+{
+    date: "2026-02-02",
+    sleepMinutes: 360,
+    meditationMinutes: 14,
+    meals: 1,
+    gym: {
+        weightKg: null,
+        reps: null
+    },
+    productivity: {
+        tasksCompleted: 3,
+        tasksSet: 3,
+        remark: "Very productive"
+    }
+},
+
+{
+    date: "2026-02-03",
+    sleepMinutes: 300,
+    meditationMinutes: 15,
+    meals: 2,
+    gym: {
+        weightKg: null,
+        reps: null
+    },
+    productivity: {
+        tasksCompleted: 1,
+        tasksSet: 4,
+        remark: "Poorly productive"
+    }
+},
+
+{
+    date: "2026-02-04",
+    sleepMinutes: 340,
+    meditationMinutes: 15,
+    meals: 1,
+    gym: {
+        weightKg: 80,
+        reps: 70
+    },
+    productivity: {
+        tasksCompleted: 3,
+        tasksSet: 4,
+        remark: "Productive"
+    }
+},
+
+{
+    date: "2026-02-05",
+    sleepMinutes: 300,
+    meditationMinutes: 10,
+    meals: 2,
+    gym: {
+        weightKg: 85,
+        reps: 160
+    },
+    productivity: {
+        tasksCompleted: 1,
+        tasksSet: 2,
+        remark: "Fairly productive"
+    }
+},
+
+{
+    date: "2026-02-06",
+    sleepMinutes: 300,
+    meditationMinutes: 15,
+    meals: 2,
+    gym: {
+        weightKg: 90,
+        reps: 110
+    },
+    productivity: {
+        tasksCompleted: 2,
+        tasksSet: 3,
+        remark: "Productive"
+    }
+},
+
+{
+    date: "2026-02-07",
+    sleepMinutes: 360,
+    meditationMinutes: 15,
+    meals: 2,
+    gym: {
+        weightKg: 85,
+        reps: 120
+    },
+    productivity: {
+        tasksCompleted: 3,
+        tasksSet: 3,
+        remark: "Very productive"
+    }
+},
+
+{
+    date: "2026-02-09",
+    sleepMinutes: 480,
+    meditationMinutes: 14,
+    meals: 2,
+    gym: {
+        weightKg: 85,
+        reps: 100
+    },
+    productivity: {
+        tasksCompleted: 3,
+        tasksSet: 4,
+        remark: "Productive"
+    }
+}]; // list of daily observations
+console.log(JSON.stringify(observations));
+showData(observations);
